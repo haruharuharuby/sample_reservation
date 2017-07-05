a13efd1fec98532dde9b3ac7b7798c938e1724a2
ynt12 <nakatake@genius-web.co.jp>
2017-07-05 17:28:22 +0900

    ページ構造の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root 'sessions#new'
+  get 'home', to:'static_pages#home'
   get '/about', to:'static_pages#about'
   get 'admins/new'
-  get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :admins

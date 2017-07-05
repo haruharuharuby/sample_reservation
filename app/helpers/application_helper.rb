c1845364d3ff8f2bc7da20c3f0f5f92e7edee01f
ynt12 <nakatake@genius-web.co.jp>
2017-07-05 10:51:43 +0900

    TOPページ制作

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #ページごとにタイトルを返す
+  def full_title(page_title='')
+    base_title = "予約システム"
+    if page_title.empty?
+      base_title
+    else
+      page_title + "|" + base_title
+    end
+  end
 end

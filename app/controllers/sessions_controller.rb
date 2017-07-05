6967c5b114e9ba27d3af4d45757f7d5a8daadd8d
ynt12 <nakatake@genius-web.co.jp>
2017-07-05 16:20:52 +0900

    ログイン中の動作追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
   def create
     admin = Admin.find_by(loginid: params[:session][:loginid].downcase)
     if admin && admin.authenticate(params[:session][:password])
+      log_in admin
+      redirect_to admin
     else
       flash.now[:danger] = 'ログインIDまたはパスワードが間違っています。'
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+
+  # 渡されたユーザーでログインする
+ def log_in(admin)
+   session[:admin_id] = admin.id
+ end
+
+ # 現在ログイン中のユーザーを返す (いる場合)
+ def current_admin
+   @current_admin ||= Admin.find_by(id: session[:admin_id])
+ end
+
+ # ログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_admin.nil?
+  end
 end

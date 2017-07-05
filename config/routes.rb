68cae6be5b0202d543327a5b3ea937dadaae731e
ynt12 <nakatake@genius-web.co.jp>
2017-07-05 14:25:55 +0900

    管理者用コントローラー等々追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'admins/new'
+
+  get 'admin/new'
+
   root 'static_pages#home'
-  get 'static_pages/about'
+  get '/about', to:'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

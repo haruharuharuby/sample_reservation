b2bef475f2979bcac51ee286aee45fbb04440a03
ynt12 <nakatake@genius-web.co.jp>
2017-08-01 18:06:30 +0900

    顧客情報更新処理追加、物販購入履歴処理追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   def index
     @staff = Staff.all
     @user = User.all
-
   end
 
   # showアクションを追加してください
@@ -14,11 +13,25 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @current_user
-      flash[:notice] = "更新しました"
-      redirect_to 'show'
+    @count = 0
+    if @current_user.tel != params[:tel]
+      @current_user.tel = params[:tel]
+      @count += 1
+    end
+    if @current_user.mail != params[:mail]
+      @current_user.mail = params[:mail]
+      @count += 1
+    end
+
+    if @count >= 1
+      if  @current_user.save
+        flash[:notice] = "更新しました"
+        redirect_to("/users/#{@current_user.id}")
+      else
+        render 'edit'
+      end
     else
-      render 'edit'
+      redirect_to("/users/#{@current_user.id}")
     end
   end
 
@@ -26,6 +39,10 @@ class UsersController < ApplicationController
   end
 
   def mathistory
+     @items= Purchase.left_outer_joins(:material)
+      .select("purchases.*, materials.*")
+      .where(purchases: {user_id: @current_user.id})
+      .order(id: :desc)
   end
 
 end

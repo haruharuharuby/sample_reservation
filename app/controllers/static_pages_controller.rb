dd1cefca1a851c3cabfbea97b096235f1eeb3dbd
ynt12 <nakatake@genius-web.co.jp>
2017-08-01 11:53:10 +0900

    施設マスタ変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
-    @facility = Facility.all
     @staff = Staff.all
     @course = Course.all
     @user = User.all
   end
+  
   def resevation
 
   end
--- db/migrate/20170706020117_create_facilities.rb
+++ db/migrate/20170706020117_create_facilities.rb
@@ -1,7 +1,6 @@
 class CreateFacilities < ActiveRecord::Migration[5.1]
   def change
     create_table :facilities do |t|
-      t.integer :facilityid
       t.string :facilityname
 
       t.timestamps
